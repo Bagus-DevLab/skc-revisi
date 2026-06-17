9bd3b6b6d8d6cde1deeffa321e64ec407a79aa41
bagus ardiansyah <bagusanardiansyah@gmail.com>
2026-06-17 23:53:41 +0700

    test: Add mock initial values for FlutterSecureStorage in widget tests

--- skillconnect_mobile/test/widget_test.dart
+++ skillconnect_mobile/test/widget_test.dart
@@ -1,9 +1,14 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:skillconnect_mobile/main.dart';
 
 void main() {
   testWidgets('renders SkillConnect mobile shell', (tester) async {
+    FlutterSecureStorage.setMockInitialValues({});
+
     await tester.pumpWidget(const SkillConnectApp());
+    await tester.pump();
+    await tester.pump(const Duration(milliseconds: 100));
 
     expect(find.text('Platform pelatihan kerja online'), findsOneWidget);
     expect(find.text('Beranda'), findsWidgets);
