e23029acebecec9b349b95e0a35bc9177df561e0
bagus ardiansyah <bagusanardiansyah@gmail.com>
2026-06-18 01:10:17 +0700

    Update API base URL to production endpoint

--- skillconnect_mobile/lib/src/config/api_config.dart
+++ skillconnect_mobile/lib/src/config/api_config.dart
@@ -3,6 +3,6 @@ class ApiConfig {
 
   static const baseUrl = String.fromEnvironment(
     'API_BASE_URL',
-    defaultValue: 'http://127.0.0.1:8000/api',
+    defaultValue: 'https://skillconnect.my.id/api',
   );
 }
